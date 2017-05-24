55855a5360d0f6f265c21133e505d9d32e216944
yvanli <yvanligo@gmail.com>
2017-05-24 11:34:01 +0800

    第一次修改
    求1-100的奇数和

---
+++ 123.java
@@ -0,0 +1,21 @@
+public class Test01 {
+
+public static void main(String[] args) {
+
+int sum = 0;
+
+for (int i = 0; i < 100; i++) {
+
+if (i%2==1) {
+
+sum += i;
+
+}
+
+}
+
+System.out.println("累加和的值" + sum);
+
+}
+
+}
